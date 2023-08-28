9ecc75f70033bb50cd2eb0099ea541c07e54c84e
marco monsanto <marco.monsanto09@gmail.com>
2023-08-28 17:06:36 +0100

    feat: add read env file, add query + parse to json

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,10 @@
 module github.com/marcomonsanto/go-discord
 
 go 1.21.0
+
+require github.com/go-sql-driver/mysql v1.7.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/guregu/null.v4 v4.0.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,65 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/marcomonsanto/go-discord/db"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/guregu/null.v4"
 )
 
+type User struct {
+	ID          int32       `json:"id"`
+	DisplayName string      `json:"display_name"`
+	AboutMe     null.String `json:"about_me"`
+	Avatar      null.String `json:"avatar"`
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbString := os.Getenv("DB_STRING")
+
+	ctx := context.Background()
+
+	database, err := sql.Open("mysql", dbString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+
+	queries := db.New(database)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintf(w, "Inside the / path")
+		dbUsers, err := queries.ListUsers(ctx)
+		var users = make([]User, 0)
+
+		for _, s := range dbUsers {
+			users = append(users, User{
+				ID:          s.ID,
+				DisplayName: s.DisplayName,
+				AboutMe:     null.NewString(s.AboutMe.String, s.AboutMe.Valid),
+				Avatar:      null.NewString(s.Avatar.String, s.Avatar.Valid),
+			})
+		}
+
+		log.Println(users)
+		if err != nil {
+			json.NewEncoder(w).Encode(err)
+			return
+		}
+		json.NewEncoder(w).Encode(users)
 	})
 
 	fmt.Println("Litening on port 8080")
